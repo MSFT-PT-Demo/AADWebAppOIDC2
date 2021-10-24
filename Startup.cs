1ed13c31423ad4456bdb4854b8867ed4b809c40a
Miguel Caldas <23634895+MiguelCaldasMS@users.noreply.github.com>
2021-10-24 15:26:07 +0100

    Minor cosmetic changes

--- Startup.cs
+++ Startup.cs
@@ -1,19 +1,16 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication;
+
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.Identity.Web;
-using Microsoft.Identity.Web.UI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AADWebAppOIDC2 {
   public class Startup {
@@ -23,14 +20,38 @@ namespace AADWebAppOIDC2 {
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services) {
-      services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme).AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"));
+      services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme).AddMicrosoftIdentityWebApp(options => {
+        Configuration.Bind("AzureAd", options);
+        // Restrict users to specific belonging to specific tenants
+        options.TokenValidationParameters.IssuerValidator = ValidateSpecificIssuers;
+      });
 
       services.AddControllersWithViews(options => {
-        var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+        AuthorizationPolicy policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
         options.Filters.Add(new AuthorizeFilter(policy));
       });
       services.AddRazorPages().AddMicrosoftIdentityUI();
     }
+
+    private string ValidateSpecificIssuers(string issuer, SecurityToken securityToken, TokenValidationParameters validationParameters) {
+      var validIssuers = GetAcceptedTenantIds().Select(tid => $"https://login.microsoftonline.com/{tid}/v2.0");
+      if (validIssuers.Contains(issuer)) {
+        return issuer;
+      } else {
+        throw new SecurityTokenInvalidIssuerException("The sign-in user's account does not belong to one of the tenants that this Web App accepts users from.");
+      }
+    }
+
+    private string[] GetAcceptedTenantIds() {
+      // If you are an ISV who wants to make the Web app available only to certain customers who are
+      // paying for the service, you might want to fetch this list of accepted tenant ids from a database.
+      // Here for simplicity we just return a hard-coded list of TenantIds.
+      return new[] {
+            "aa3efc31-e34b-4415-9c6f-aae13213cf33", // caldas.dev
+            "43d106cd-a001-4919-a26f-41c820de286c" // caldas.eu
+      };
+    }
+
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
